7205fe9835c6f770f3110c14dcb62fa82688ccee
Achok-kot <p.kot@alustudent.com>
2026-06-10 15:48:08 +0200

    componets Dashboard, Navbar, UseCrd created

---
+++ src/components/DashboardStats.jsx
@@ -0,0 +1,13 @@
+export default function DashboardStats() {
+    return (
+        <section>
+            <h2>Dashboard Statistics</h2>
+
+            <div>
+                <p>total tasks: 24</p>
+                 <p>completoed: 18</p>
+                 <p>pending Tasks: 6</p>
+            </div>
+        </section>
+    );
+}
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,8 @@
+export default function UserCard() {
+    return (
+        <div>
+            <h3>Panom Achok</h3>
+            <p>Fullstack Software Developer</p>
+        </div>
+    );
+}
